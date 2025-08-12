643d09ee2d2ef5af157cfdd9e646c1bc123985c2
Moiseev Ilya <ilya@mois.pro>
2025-08-12 10:41:59 +0400

    fix(cell): update operation identifier for invoice payload and comment out unused jetton payload code

--- apps/demo-dapp/src/components/cell.ts
+++ apps/demo-dapp/src/components/cell.ts
@@ -10,20 +10,23 @@ const tonsiteAdnl = beginCell()
     .storeBuffer(exampleAdnlBuffer); // Placeholder for ADNL address
 
 const invoicePayload = beginCell()
-    .storeUint(0x7aa23eb5, 32) // JETTON_TRANSFER_OP_CODE (operation identifier)
+    .storeUint(0x7aa23eb5, 32) // INVOICE_OP_CODE (operation identifier)
     .storeBuffer(uuid)
     .storeBuilder(noneAdnl)
     .endCell();
 
 const invoiceTonPayload = invoicePayload;
 
-const invoiceJettonPayload = beginCell()
-    .storeUint(0xf8a7ea5, 32) // JETTON_TRANSFER_OP_CODE (operation identifier)
-    .storeUint(0, 64) // Query ID (0 for new transactions)
-    .storeCoins(jettonAmount) // Amount to transfer (1 USDt)
-    .storeAddress(destination) // Recipient address
-    .storeAddress(wallet.address) // Address to receive excess funds (usually address of sender)
-    .storeBit(false) // No custom payload
-    .storeCoins(1n) // Forward fee in nanoTON (for send notify to wallet)
-    .storeMaybeRef(invoicePayload) // Forward payload
-    .endCell();
+// const invoiceJettonPayload = beginCell()
+//     .storeUint(0xf8a7ea5, 32) // JETTON_TRANSFER_OP_CODE (operation identifier)
+//     .storeUint(0, 64) // Query ID (0 for new transactions)
+//     .storeCoins(jettonAmount) // Amount to transfer (1 USDt)
+//     .storeAddress(destination) // Recipient address
+//     .storeAddress(wallet.address) // Address to receive excess funds (usually address of sender)
+//     .storeBit(false) // No custom payload
+//     .storeCoins(1n) // Forward fee in nanoTON (for send notify to wallet)
+//     .storeMaybeRef(invoicePayload) // Forward payload
+//     .endCell();
+
+console.log("invoicePayload", invoicePayload.toBoc().toString("base64"));
+// console.log("invoiceTonPayload", invoiceTonPayload.toBoc().toString("base64"));
